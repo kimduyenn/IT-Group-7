278bac647c7e5f482531e2539d688e509e83bda0
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-16 16:40:11 +0700

    Update pythonnnn.py

--- pythonnnn.py
+++ pythonnnn.py
@@ -6,17 +6,17 @@ from matplotlib.colors import LinearSegmentedColormap
 
 athlete_events = pd.read_csv('Book1.csv')
 
-selected_sports = ["Athletics", "Badminton", "Boxing", "Cycling", "Gymnastics", "Swimming"]
-my_data = athlete_events[(athlete_events['Year'] == 2016) & (athlete_events['Sport'].isin(selected_sports))]
+b_data = athlete_events[athlete_events['Sport'].isin(["Boxing", "Football", "Judo", "Swimming", "Taekwondo"])]
 
-sport_counts = my_data['Sport'].value_counts(normalize=True) * 100
+athlete_counts = b_data['Sport'].value_counts().reset_index()
+athlete_counts.columns = ['Sport', 'count']
 
 plt.figure(figsize=(8, 8))
-plt.pie(sport_counts, labels=sport_counts.index, autopct='%1.1f%%', startangle=140)
-plt.axis('equal') # Equal aspect ratio ensures that pie is drawn as a circle.
-
+plt.pie(athlete_counts['count'], labels=athlete_counts['Sport'], autopct='%1.1f%%', startangle=90, colors=plt.cm.tab10.colors, wedgeprops=dict(width=0.3))
+plt.gca().add_patch(plt.Circle((0, 0), 0.5, color='white'))
+plt.title('The number of athletes participating in the Olympic during 120 Years')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
-
 
 
 
