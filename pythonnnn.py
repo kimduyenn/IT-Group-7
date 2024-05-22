fbadc7a58b3938ea8eee14a23c20bb984f0c25f8
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-23 03:47:47 +0700

    Update pythonnnn.py

--- pythonnnn.py
+++ pythonnnn.py
@@ -1,5 +1,19 @@
 import pandas as pd
 pip install openpyxl
+import streamlit as st
+import pandas as pd
+
+# Define the URL of the Excel file on GitHub
+file_url = "https://github.com/kimduyenn/Python/raw/main/Athlete_events.xlsx"
+
+# Read the Excel file
+try:
+    athlete_events = pd.read_excel(file_url)
+except Exception as e:
+    st.error(f"An error occurred while reading the Excel file: {e}")
+
+# Display the data
+st.write(athlete_events)
 
 # Specify the file path
 file_path = r"C:\Users\admin\OneDrive\文档\GitHub\Python\Athlete_events.xlsx"
