868869fa4cddeac2b9b08778a0f65369f51ea594
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-23 03:10:01 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+# Title
+st.title('My First Streamlit App')
+
+# Load Excel file
+try:
+    athlete_events = pd.read_excel('Athlete_events.xlsx')
+    st.write(athlete_events.head())
+except FileNotFoundError:
+    st.error("File not found. Please make sure the file 'Athlete_events.xlsx' exists in the same directory as this script.")
