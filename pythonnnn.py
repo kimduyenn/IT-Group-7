588c52bd8eb992b416e90bdf5adacf1cf3c1b340
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-29 22:59:50 +0700

    Update pythonnnn.py

--- pythonnnn.py
+++ pythonnnn.py
@@ -2,26 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
 import numpy as np
-import seaborn as sns
 import geopandas as gpd
 from matplotlib.colors import LinearSegmentedColormap
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import plotly.express as px
+import streamlit as st
+athlete_events = pd.read_excel(r'D:\Athlete_events.xlsx')
 
-athlete_events = pd.read_excel('Athlete_events.xlsx')
 
 # PLOT 1
 
+
+# Filter data for the selected sports in 2016
 selected_sports = ["Athletics", "Badminton", "Boxing", "Cycling", "Gymnastics", "Swimming"]
 my_data = athlete_events[(athlete_events['Year'] == 2016) & (athlete_events['Sport'].isin(selected_sports))]
 
+# Calculate the percentage of athletes in each sport
 sport_counts = my_data['Sport'].value_counts(normalize=True) * 100
 
-explode = [0.02] * len(sport_counts) 
+# Create the pie chart with Plotly Express
+fig = px.pie(values=sport_counts, names=sport_counts.index, title='Distribution of Athletes in Selected Sports (2016)', hole=0.1)
+fig.update_traces(textposition='inside', textinfo='percent+label')
 
-plt.figure(figsize=(8, 8))
-plt.pie(sport_counts, labels=sport_counts.index, autopct='%1.1f%%', startangle=140, explode=explode)
-plt.axis('equal') 
-plt.show()
+# Display the chart in Streamlit
+st.plotly_chart(fig)
 
 
