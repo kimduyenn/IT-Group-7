89c1594a29c8238ec1cc64009fafae69995e61fd
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-30 01:27:49 +0700

    Update pythonnnn.py

--- pythonnnn.py
+++ pythonnnn.py
@@ -13,13 +13,13 @@ athlete_events = pd.read_excel(r'Athlete_events.xlsx')
 st.write(athlete_events) 
 
 # PLOT 1
-
+st.write(Distribution of Athletes in Selected Sports (2016))
 selected_sports = ["Athletics", "Badminton", "Boxing", "Cycling", "Gymnastics", "Swimming"]
 my_data = athlete_events[(athlete_events['Year'] == 2016) & (athlete_events['Sport'].isin(selected_sports))]
 
 sport_counts = my_data['Sport'].value_counts(normalize=True) * 100
 
-fig = px.pie(values=sport_counts, names=sport_counts.index, title='Distribution of Athletes in Selected Sports (2016)', hole=0.1)
+fig = px.pie(values=sport_counts, names=sport_counts.index, hole=0.1)
 fig.update_traces(textposition='inside', textinfo='percent+label')
 
 st.plotly_chart(fig)
