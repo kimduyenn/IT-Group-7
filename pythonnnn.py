b96310caea19a947f3451b958c17b9ea432abcc2
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-29 23:08:36 +0700

    Update pythonnnn.py

--- pythonnnn.py
+++ pythonnnn.py
@@ -13,19 +13,14 @@ athlete_events = pd.read_excel(r'Athlete_events.xlsx')
 
 # PLOT 1
 
-
-# Filter data for the selected sports in 2016
 selected_sports = ["Athletics", "Badminton", "Boxing", "Cycling", "Gymnastics", "Swimming"]
 my_data = athlete_events[(athlete_events['Year'] == 2016) & (athlete_events['Sport'].isin(selected_sports))]
 
-# Calculate the percentage of athletes in each sport
 sport_counts = my_data['Sport'].value_counts(normalize=True) * 100
 
-# Create the pie chart with Plotly Express
 fig = px.pie(values=sport_counts, names=sport_counts.index, title='Distribution of Athletes in Selected Sports (2016)', hole=0.1)
 fig.update_traces(textposition='inside', textinfo='percent+label')
 
-# Display the chart in Streamlit
 st.plotly_chart(fig)
 
 
