e5affc692c1da8fb72575bf3a56718b0d210bbcf
kimduyenn <10323044@student.vgu.edu.vn>
2024-05-30 15:27:19 +0700

    Update Introduction_our_team.py

--- Introduction_our_team.py
+++ Introduction_our_team.py
@@ -1,3 +1,4 @@
+# Import stoggle function
 import streamlit as st 
 import pandas as pd 
 import numpy as np
@@ -11,15 +12,6 @@ from streamlit_extras.stoggle import stoggle
 from streamlit_extras.let_it_rain import rain
 import matplotlib.pyplot as plt
 import plotly.express as px
-
-# Page configuration
-st.set_page_config(page_title="PYTHON 2 - BUSINESS IT 2", page_icon="🥰", layout="wide")
-
-# Header and description
-st.subheader("Group 7")
-st.title("PYTHON 2 - BUSINESS IT 2 :orange_heart:")
-st.write("We are a group of business students who are interested in the economical situation in the world. Therefore, we decided to analyze a set of data about the employment fluctuation in the USA from 1978 to 2022. Through this visualization, we hope to bring a clear vision to people about how the labor market in the USA has changed over the past decades.")
-
 # Group information toggle
 stoggle(
     "Group information",
@@ -46,60 +38,27 @@ colored_header(
     description="Get to know about our group",
     color_name="light-blue-70",
 )
-
 # Group member 1
 tv2 = st.columns(2)
-
-with tv2:
-    st.subheader("**Full name: Dinh Ha Tu Van (Group leader)**")
-    st.write("Student ID: 10622045")
-    st.write("Email: 10622045@student.vgu.edu.vn")
-    st.write("Major: Business Administration (BBA)")
-    st.write("Phone number: 077 6209215")
-
-st.write(" ")
+st.subheader("**Full name: Dinh Ha Tu Van (Group leader)**")
+st.write("Student ID: 10622045")
+st.write("Email: 10622045@student.vgu.edu.vn")
+st.write("Major: Business Administration (BBA)")
+st.write("Phone number: 077 6209215")
 
 # Group member 2
 hq2 = st.columns(2)
-
-with hq2:
-    st.subheader("**Full name: Bui Cam Ha Quyen**")
-    st.write("Student ID: 10622023")
-    st.write("Email: 10622023@student.vgu.edu.vn")
-    st.write("Major: Finance & Accounting (BFA)")
-    st.write("Phone number: 090 8784370")
-
-st.write(" ")
+st.subheader("**Full name: Bui Cam Ha Quyen**")
+st.write("Student ID: 10622023")
+st.write("Email: 10622023@student.vgu.edu.vn")
+st.write("Major: Finance & Accounting (BFA)")
+st.write("Phone number: 090 8784370")
 
 # Group member 3
 mh2 = st.columns(2)
-
-with mh2:
-    st.subheader("**Full name: Mai Hong Hanh**")
-    st.write("Student ID: 10622014")
-    st.write("Email: 10622014@student.vgu.edu.vn")
-    st.write("Major: Business Administration (BBA)")
-    st.write("Phone number: 039 2230636")
-
+st.subheader("**Full name: Mai Hong Hanh**")
+st.write("Student ID: 10622014")
+st.write("Email: 10622014@student.vgu.edu.vn")
+st.write("Major: Business Administration (BBA)")
+st.write("Phone number: 039 2230636")
 st.markdown("---")
-
-# Contact form
-st.subheader("💬 Leave us your message!")
-st.caption("Let us know if you have any recommendations")
-
-contactform = """
-<form action="https://formsubmit.co/10622045@student.vgu.edu.vn" method="POST">
-    <input type="hidden" name="_captcha" value="false">
-    <input type="text" name="name" placeholder="Your name" required>
-    <input type="email" name="email" placeholder="Your email address" required>
-    <textarea name="message" placeholder="What do you think?"></textarea>
-    <button type="submit">Send</button>
-</form>
-"""
-st.markdown(contactform, unsafe_allow_html=True)
-
-# Local CSS function
-def local_css(style):
-    with open(style) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("style.css")
