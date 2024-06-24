740c18bf79fb3fc6f4949062f70546d0502c10f9
kimduyenn <10323044@student.vgu.edu.vn>
2024-06-24 16:17:02 +0700

    Add files via upload

--- Introduction_our_team.py
+++ Introduction_our_team.py
@@ -17,25 +17,26 @@ st.set_page_config(page_title="PYTHON 2 - BUSINESS IT 2", page_icon="🥰", layo
 
 # Header and description
 st.subheader("Group 7")
-st.title("PYTHON 2 - BUSINESS IT 2 :orange_heart:")
+st.title("PYTHON 2 - BUSINESS IT 2 :blue_heart:")
 st.write("We are a group of business students who are interested in the economical situation in the world. Therefore, we decided to analyze a set of data about the employment fluctuation in the USA from 1978 to 2022. Through this visualization, we hope to bring a clear vision to people about how the labor market in the USA has changed over the past decades.")
 
 # Group information toggle
 stoggle(
     "Group information",
     """
-    1. Dinh Ha Tu Van - 10622045
-    2. Bui Cam Ha Quyen - 10622023
-    3. Mai Hong Hanh - 10622014
-    4. 
-    """
-)
 
-st.write("[Accessing our dataset >](https://docs.google.com/spreadsheets/d/1HbBDpeXYXhl3MQU2bZ-YZiHb1Fe2COrT/edit?usp=drive_link&ouid=114022649098492793407&rtpof=true&sd=true)")
+ 1. Tran Thi Thuy Trang - 10323060
+
+ 2. Tran Ngoc My Thao - 10323059
+
+ 3. Luong Nu Mai Nhung - 10323056
+
+ 4. Kim Duyen - 10323044 """,)
+st.write("[Accessing our dataset >](https://python-itpython2group7.streamlit.app/)") 
 
 # Adding rain effect
 rain(
-    emoji="❤️",
+    emoji="💕",
     font_size=54,
     falling_speed=5,
     animation_length="3",
@@ -50,26 +51,30 @@ colored_header(
 
 # Group member 1
 tv2 = st.columns(2)
-st.subheader("**Full name: Dinh Ha Tu Van (Group leader)**")
-st.write("Student ID: 10622045")
-st.write("Email: 10622045@student.vgu.edu.vn")
-st.write("Major: Business Administration (BBA)")
-st.write("Phone number: 077 6209215")
+st.subheader("**Full name: Tran Thi Thuy Trang**")
+st.write("Student ID: 10323060")
+st.write("Email: 10323060@student.vgu.edu.vn")
+st.write("Major: Finance & Accounting (BFA)")
 
 # Group member 2
 hq2 = st.columns(2)
-st.subheader("**Full name: Bui Cam Ha Quyen**")
-st.write("Student ID: 10622023")
-st.write("Email: 10622023@student.vgu.edu.vn")
+st.subheader("**Full name: Tran Ngoc My Thao**")
+st.write("Student ID: 10323059")
+st.write("Email: 10323059@student.vgu.edu.vn")
 st.write("Major: Finance & Accounting (BFA)")
-st.write("Phone number: 090 8784370")
 
 # Group member 3
 mh2 = st.columns(2)
-st.subheader("**Full name: Mai Hong Hanh**")
-st.write("Student ID: 10622014")
-st.write("Email: 10622014@student.vgu.edu.vn")
-st.write("Major: Business Administration (BBA)")
-st.write("Phone number: 039 2230636")
+st.subheader("**Full name: Luong Nu Mai Nhung**")
+st.write("Student ID: 10323056")
+st.write("Email: 10323056@student.vgu.edu.vn")
+st.write("Major: Finance & Accounting (BFA)")
+
+# Group member 4
+mh2 = st.columns(2)
+st.subheader("**Full name: Kim Duyen**")
+st.write("Student ID: 10323044")
+st.write("Email: 10323044@student.vgu.edu.vn")
+st.write("Major: Finance & Accounting (BFA)")
 st.markdown("---")
 
